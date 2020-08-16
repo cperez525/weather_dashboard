37a76622bda776077657bbcf57a328800356cd65
Cris Perez <cristino.perez525@gmail.com>
2020-08-16 14:55:13 -0500

    Added, tested, and confirmed desired api call url

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var currentDay = moment().format("MM/DD/YYYY")
-console.log(currentDay)
+function setInfo() {
+
+    var apiKey = "eb427715aaf53ff889a6c5764adf94e1"
+    var cityName = $("#text-area").val()
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=6&appid=" + apiKey
+
+    $.ajax({
+        url: weatherUrl,
+        method: "GET"
+    }).then(function (response) {
+        
+    })
+}
+
+$("#search-btn").on("click", function () {
+    setInfo()
+})
+
